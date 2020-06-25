375dee38ba2f8da1233ee64b54d8f9688910f571
ignacio_345@hotmail.com <ignacio_345@hotmail.com>
2020-06-25 13:16:37 -0500

    Project 16 Added

---
+++ Project16/mapIt.py
@@ -0,0 +1,19 @@
+
+# Developer: Ignacio Aular
+# ignacio345@gmail.com
+# Thu Jun 25, 1:15 PM
+
+#! python3
+# mapIt.py - Launches a map in the browser using an address from the
+# command line or clipboard.
+
+import webbrowser, sys, pyperclip
+
+if len(sys.argv) > 1:
+    # Get address from command line.
+    address = ' '.join(sys.argv[1:])
+else:
+    # Get address from clipboard.
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
