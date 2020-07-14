28de46112c13fa8511b54f95d2d8509319ffa79f
ignacio_345@hotmail.com <ignacio_345@hotmail.com>
2020-07-13 20:13:51 -0500

    Project 23 Added

---
+++ Project23/cellInverter.py
@@ -0,0 +1,35 @@
+
+
+# Developer: Ignacio Aular
+# ignacio345@gmail.com
+# Mon JuL 13, 8:11 PM
+
+#! python3
+# cellInverter.py
+
+import openpyxl
+
+def invertCells(filename):
+    """inverts all cells in a workbook
+    Args:
+        filename (str): excel file to invert cells in
+    Returns:
+        None
+    """
+    wb = openpyxl.load_workbook(filename)
+    sheet = wb.active
+    newSheet = wb.create_sheet(index=0, title='inverted')
+
+    for rowObj in sheet.rows:
+        for cellObj in rowObj:
+            colIndex = cellObj.column
+            rowIndex = cellObj.row
+
+            newSheet.cell(row=colIndex, column=rowIndex).value = cellObj.value
+
+    wb.save('result_'+filename)
+
+
+if __name__ == "__main__":
+    invertCells('example.xlsx')
+
